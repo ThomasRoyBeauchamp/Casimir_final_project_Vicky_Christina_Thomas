7423158ef1b50a9a44644f539f3a8ad49f33a97c
tbeauchamp <thomasroybeauchamp@gmail.com>
2023-05-22 17:00:21 +0200

    Updated to get list of dodgy conferences

--- Final_project_Vicky_Christina_Thomas.py
+++ Final_project_Vicky_Christina_Thomas.py
@@ -14,6 +14,8 @@ import smtplib
 import requests
 import os
 
+from Conference_Hunting import Conference, get_all_conferences
+
 from key_words import key_authors, key_words, key_email_addresses
 
 TO = key_email_addresses #TO = ["J.M.Brevoord@tudelft.nl", "M.Roehsner@tudelft.nl","julius.fischer@tudelft.nl"]
@@ -46,106 +48,9 @@ def get_computer_name():
     return pwd.getpwuid(os.getuid())[0]
 
 
-def find_favourite_papers(articles, favourites, html_class, rank_list):
-    """Generates a rank list of indices of the articles according to the specified favourites list
-    
-    Uses BeautifulSoup methods to find the favourites
-
-    Parameters
-    ----------
-    articles : BeautifulSoup object
-        all articles we want to search
-    favourites : list of strings
-        favourites we search for
-    html_class : str
-        defines html class we search in
-    rank_list : list of int
-        list that the favourites are appended to
-    
-    Returns
-    -------
-    rank_list : list of int
-        list with appended favourites
-    """
-    for i,div in enumerate(articles):
-        # filter that we use
-        div_class = div.find("p", {'class': html_class})
-        for favourite in favourites:
-            if favourite.lower() in str(div_class).lower():
-                rank_list.append(i)
-    return rank_list
+all_dodgy_conferences = get_all_conferences()
 
 
-if date.today().weekday() == 0:  # today is Monday
-    day = date.today() - timedelta(days=4)
-else:  # every other day
-    day = date.today() - timedelta(days=2)
-
-
-# Download the page
-url = search_string.format(day)
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-
-
-# Remove unnecessary stuff
-soup.find("header").decompose()
-soup.find("footer").decompose()
-for div in soup.find_all("div", {'class': 'level is-marginless'}):
-    div.decompose()
-for div in soup.find_all("div", {'class': 'level-right'}):
-    div.decompose()
-for div in soup.find_all("div", {'class': 'columns'}):
-    div.decompose()
-for div in soup.find_all("div", {'class': 'level breathe-horizontal'}):
-    div.decompose()
-for div in soup.find_all("div", {'class': 'is-hidden-tablet'}):
-    div.decompose()
-for div in soup.find_all("span", {'class': 'abstract-short'}):
-    div.decompose()
-for div in soup.find_all("a", {'class': 'is-size-7'}):
-    div.decompose()
-for div in soup.find_all("span", {'class': 'abstract-full'}):
-    div['style'] = ""
-
-
-# Add title
-soup.find("ol", {'class': 'breathe-horizontal'}).contents[0].replaceWith(
-    BeautifulSoup(
-        '''
-        <h1 class="title">Your daily arXiv update {}</h1>
-        <p>Source code is in the arxiv email server repository.</p>
-        <p>This script is currently executed by {}.</p>
-        <h1 class="title"> <center> --- Suggestion Section --- </center> </h1>
-        '''
-        .format(date.today(), get_computer_name()), features='html.parser'
-    ))
-
-
-# Reorder articles according to favourite keyword lists
-
-# get all articles
-all_articles = soup.find_all("li", {'class': 'arxiv-result'})
-current_first_article = all_articles[0]
-
-# find articles in all articles with matching keyword from favourite list
-ranking = []
-ranking = find_favourite_papers(all_articles, favourite_authors, 'authors', ranking)
-ranking = find_favourite_papers(all_articles, favourite_words, 'title is-5 mathjax', ranking)
-ranking = find_favourite_papers(all_articles, favourite_words, 'abstract mathjax', ranking)
-
-ranking = list(set(ranking)) # get rid of multiple entries
-ranking.sort() # sort the entries
-
-# shuffel the favourites to the top according to the ranking list
-for i in ranking:
-    if i != 0:
-        current_first_article.insert_before(all_articles[i])
-        current_first_article = all_articles[i]
-
-# insert text that indicates the end of our suggested papers
-all_articles = soup.find_all("li", {'class': 'arxiv-result'})
-all_articles[len(ranking)].insert_before(BeautifulSoup('<h1 class="title"> <center> --- End Suggestion Section --- </center> </h1>', features='html.parser'))
 
 soup=f'''Dear Julius,
 
@@ -169,7 +74,7 @@ Vicky, Thomas and Christina'''
 #print(soup, 'html')
 
 # Send email
-SUBJECT = "Your favourite dodgy confereces".format(date.today())
+SUBJECT = "Your favourite dodgy conferences".format(date.today())
 FROM = "c.i.ioannou@tudelft.nl"
 
 server = smtplib.SMTP('smtp.tudelft.nl')
