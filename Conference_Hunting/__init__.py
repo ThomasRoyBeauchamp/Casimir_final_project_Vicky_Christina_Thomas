be5afb91097907a1c7b53dcc068a805dbf5d6653
tbeauchamp <thomasroybeauchamp@gmail.com>
2023-05-22 11:41:25 +0200

    Changed Conference class to be initialised from the <li> entry on the summary page. Added method to resiliently do HTTP requests

--- Conference_Hunting/__init__.py
+++ Conference_Hunting/__init__.py
@@ -24,7 +24,7 @@ MONTH_DICT = {
     "DECEMBER": 12
 }
 
-def get_conference_pages_from_webpage(raw_html):
+def get_conferences_from_webpage(raw_html):
 
     website = BeautifulSoup(raw_html, "html.parser")
 
@@ -33,10 +33,16 @@ def get_conference_pages_from_webpage(raw_html):
 
     eventList = website.find("div", {"id": "eventList"})
 
-    try:
-        return [c.attrs["href"] for c in eventList.find_all("a") if "title" in c.attrs.keys()]
-    except:
-        return []
+    conference_entries = [x for y in eventList.find_all("ul", {"class":"list-unstyled"}) for x in y.find_all("li")]
+
+    year_mapping = dict([tuple(x.strong.text.split(',')) for x in eventList.find_all("div", {"class": "card-header"})])
+
+    for k in year_mapping.keys():
+        year_mapping[k] = int(year_mapping[k])
+
+
+    return [Conference(d, year_mapping) for d in conference_entries]
+
 
 def get_all_conference_pages():
 
@@ -51,7 +57,7 @@ def get_all_conference_pages():
 
         if http_request.status_code == 200:
 
-            new_conference_pages = get_conference_pages_from_webpage(http_request.content)
+            new_conference_pages = get_conferences_from_webpage(http_request.content)
 
             if new_conference_pages:
                 pages += new_conference_pages ##adds the new conferences to the list of all conferences
@@ -64,75 +70,108 @@ def get_all_conference_pages():
 
     return pages
 
+def do_html_request(website: str) -> (bool, BeautifulSoup | None):
+    """
+    Handles the HTTP request.
+    :param website: Website to retrieve
+    :return: (Successful, Website as BeautifulSoup or None)
+    """
+    try:
+        html_request = requests.get(website)
 
+        if html_request.status_code == 200:
+            return True, BeautifulSoup(html_request.content)
+        else:
+            return False, None
+
+    except:
+
+        return False, None
 
 class Conference:
 
-    def __init__(self, webpage):
+    def __init__(self, div: bs4.Tag, year_mapping: dict):
 
-        self.did_load = False
+        _text = div.text.split("\n")
+        while '' in _text:
+            _text.remove('')
 
-        website = BeautifulSoup(requests.get(webpage).content, 'html.parser')
+        self.name = _text[1].lstrip()
 
-        website.find("header").decompose()
-        website.find("footer").decompose()
+        self.location = _text[2].lstrip().replace("- ", "")
 
-        a: bs4.NavigableString
+        _date = _text[0].lstrip().split(' ')
+        self.date = date(year=year_mapping[_date[0]], month=MONTH_DICT[_date[0].upper()], day=int(_date[1]))
 
-        _desc = website.find("div", {"id": "event-description"}) ##Finds the event description tag
-        if _desc is not None:
-            for b in _desc.find_all("br"):  ##Converts <br> tags to \n
-                b.replaceWith(" | ")
+        self.website = div.a.attrs["href"]
 
-            self.description = _desc.text.lower()
-                # .split('\n')  # Converts contents to lower case
-            # if '' in self.description:
-            #     self.description.remove('')
-            # self.description[0] = self.description[0].lstrip()
+        pass
 
-        else:
-            self.description = []
-
-
-        self.title = website.find("div", {"class": "col-lg-9 col-sm-12"}).find("h1").text
-
-
-
-        self.tags = ' | '.join([a.text for a in website.find("li", {"class": "mt-3"}).find_all('a')]) ##tags contained in a li tag with class mt-3
-
-        details_container = website.find("ul", {"class": "mb-2 list-unstyled"})
-
-        lines = details_container.find_all("li")
-
-        self.attrs = dict([tuple(l.text.split(":", 1)) for l in lines if "class" not in l.attrs.keys()])
-        ## Contains everything in the Shortname, type, URLs etc.
-
-        if "Date" in self.attrs.keys():
-
-            _date = self.attrs["Date"].split(" ")
-
-            self.date = date(day=int(_date[2].split('-')[0]) if '-' in _date[2] else int(_date[2]), month=MONTH_DICT[_date[1].upper()], year = int(_date[3]))
-
-        else:
-            self.date = date(year=0, month=0, day=0)
-
-        if not (date.today() + timedelta(days=60) < self.date < date.today() + timedelta(days=6*30)):
-            return
-
-
-        self._keywords = [kwd for kwd in KEYWORDS if kwd in self.tags or kwd in self.description]
-
-
-
-        if "Program URL" in self.attrs.keys() and self._keywords: #Only looks for speakers if there are keywords
-            program_page = BeautifulSoup(requests.get(self.attrs["Program URL"]).content, 'html.parser').text
-
-            self._speakers = [spk for spk in SPEAKERS if spk in program_page]
-        else:
-            self._speakers = []
-
-
-        self.did_load = True
+    # def __init__(self, webpage):
+    #
+    #     self.did_load = False
+    #
+    #     website = BeautifulSoup(requests.get(webpage).content, 'html.parser')
+    #
+    #     website.find("header").decompose()
+    #     website.find("footer").decompose()
+    #
+    #     a: bs4.NavigableString
+    #
+    #     _desc = website.find("div", {"id": "event-description"}) ##Finds the event description tag
+    #     if _desc is not None:
+    #         for b in _desc.find_all("br"):  ##Converts <br> tags to \n
+    #             b.replaceWith(" | ")
+    #
+    #         self.description = _desc.text.lower()
+    #             # .split('\n')  # Converts contents to lower case
+    #         # if '' in self.description:
+    #         #     self.description.remove('')
+    #         # self.description[0] = self.description[0].lstrip()
+    #
+    #     else:
+    #         self.description = []
+    #
+    #
+    #     self.title = website.find("div", {"class": "col-lg-9 col-sm-12"}).find("h1").text
+    #
+    #
+    #
+    #     self.tags = ' | '.join([a.text for a in website.find("li", {"class": "mt-3"}).find_all('a')]) ##tags contained in a li tag with class mt-3
+    #
+    #     details_container = website.find("ul", {"class": "mb-2 list-unstyled"})
+    #
+    #     lines = details_container.find_all("li")
+    #
+    #     self.attrs = dict([tuple(l.text.split(":", 1)) for l in lines if "class" not in l.attrs.keys()])
+    #     ## Contains everything in the Shortname, type, URLs etc.
+    #
+    #     if "Date" in self.attrs.keys():
+    #
+    #         _date = self.attrs["Date"].split(" ")
+    #
+    #         self.date = date(day=int(_date[2].split('-')[0]) if '-' in _date[2] else int(_date[2]), month=MONTH_DICT[_date[1].upper()], year = int(_date[3]))
+    #
+    #     else:
+    #         self.date = date(year=0, month=0, day=0)
+    #
+    #     if not (date.today() + timedelta(days=60) < self.date < date.today() + timedelta(days=6*30)):
+    #         return
+    #
+    #
+    #     self._keywords = [kwd for kwd in KEYWORDS if kwd in self.tags or kwd in self.description]
+    #
+    #
+    #
+    #     if "Program URL" in self.attrs.keys() and self._keywords: #Only looks for speakers if there are keywords
+    #         program_page = BeautifulSoup(requests.get(self.attrs["Program URL"]).content, 'html.parser').text
+    #
+    #         self._speakers = [spk for spk in SPEAKERS if spk in program_page]
+    #     else:
+    #         self._speakers = []
+    #
+    #
+    #     self.did_load = True
 
 
 
